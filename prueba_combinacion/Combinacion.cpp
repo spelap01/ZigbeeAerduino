fc89c80ababc3533f17bad35e58fb53cd1818cdf
spelap01 <spelap01@estudiantes.unileon.es>
2021-05-09 19:58:47 +0200

    Combinacion terminada
    clase combinacion terminada, hago la combinacion y la devuelvo como bytes

--- prueba_combinacion/Combinacion.cpp
+++ prueba_combinacion/Combinacion.cpp
@@ -8,7 +8,66 @@ void Combinacion::incrementaComb(int* comb,int valorPos, int pos){
     comb[pos]=valorPos;
     ///holahola
 }
-void Combinacion::incrementaCombprueba(int* comb,int valorPos, int pos){
-   DEBUG_PRINT(0xA2);
-   incrementaComb(comb,valorPos,pos);
+byte Combinacion::generarByte(int valor1, int valor2){
+  char aux1=cambiarHex(valor1);
+  char aux2=cambiarHex(valor2);
+  char textoaux[2];
+  textoaux[0]=aux1;
+  textoaux[1]=aux2;
+  unsigned long auxDevolver = strtoul(textoaux, NULL, HEX);
+  return auxDevolver;
+}
+char Combinacion::cambiarHex(int valor){
+  char devolver;
+      switch(valor){
+        case 0:
+          devolver='0';
+        break;
+        case 1:
+          devolver='1';
+        break;
+        case 2:
+          devolver='2';
+        break;
+        case 3:
+          devolver='3';
+        break;
+        case 4:
+          devolver='4';
+        break;
+        case 5:
+          devolver='5';
+        break;
+        case 6:
+          devolver='6';
+        break;
+        case 7:
+          devolver='7';
+        break;
+        case 8:
+          devolver='8';
+        break;
+        case 9:
+          devolver='9';
+        break;
+        case 10:
+          devolver='A';
+        break;
+        case 11:
+          devolver='B';
+        break;
+        case 12:
+          devolver='C';
+        break;
+        case 13:
+          devolver='D';
+        break;
+        case 14:
+          devolver='E';
+        break;
+        case 15:
+          devolver='F';
+        break;
+    }
+  return devolver;
 }

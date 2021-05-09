d9e5bfbbb60f9d88a72b862b17efca1db0543af8
spelap01 <spelap01@estudiantes.unileon.es>
2021-05-09 19:22:55 +0200

    prueba de branches
    pruebas

--- prueba_combinacion/Combinacion.cpp
+++ prueba_combinacion/Combinacion.cpp
@@ -6,6 +6,7 @@ void Combinacion::incrementaComb(int* comb,int valorPos, int pos){
       //incrementaComb(comb[pos-1],pos-1);
     }
     comb[pos]=valorPos;
+    ///holahola
 }
 void Combinacion::incrementaCombprueba(int* comb,int valorPos, int pos){
    DEBUG_PRINT(0xA2);
